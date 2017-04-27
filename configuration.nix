8b5d7f6514bd2808f2cc7b137a85ce155b07008e
David Asabina <david@supr.nu>
2017-04-28 01:30:40 +0200

    fix synaptic to synaptics

--- configuration.nix
+++ configuration.nix
@@ -77,7 +77,7 @@
     };
     enable = true;
     layout = "us";
-    synaptic = {
+    synaptics = {
       enable = true;
     };
     videoDrivers = ["intel"];
@@ -98,12 +98,15 @@
   system.stateVersion = "17.03";
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.extraUsers.vid = {
-    isNormalUser = true;
-    # uid = 1988;
-    description = "David Asabina";
-    extraGroups = [ "wheel" "networkmanager" ];
-    initialPassword = "#unsecure";
+  users {
+    defaultUserShell = "/run/current-system/sw/bin/zsh";
+    extraUsers.vid = {
+      isNormalUser = true;
+      # uid = 1988;
+      description = "David Asabina";
+      extraGroups = [ "wheel" "networkmanager" ];
+      initialPassword = "#unsecure";
+    };
   };
 
   # Set your time zone.
