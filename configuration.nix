b8dc0ddfbf24df7b6af7b01d93811dfebc727d08
David Asabina <david@supr.nu>
2017-04-29 19:30:14 +0200

    taking care of the stringAfter var

--- configuration.nix
+++ configuration.nix
@@ -2,9 +2,10 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, pkgs, ... }:
+{ config, lib, pkgs, ... }:
 
-{
+with lib;
+rec {
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
