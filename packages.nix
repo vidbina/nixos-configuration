ce500d7386a152cea80968c97498c63e25ed2aee
David Asabina <vid@bina.me>
2017-05-14 04:53:11 +0200

    [ps]tree + tmux + conky

--- packages.nix
+++ packages.nix
@@ -1,15 +1,7 @@
 { config, pkgs, ... }:
-
-{
-  # List packages installed in system profile. To search by name, run:
-  # $ nix-env -qaP | grep wget
   environment.systemPackages = with pkgs; [
     chromium
-    # perhaps I need a namesake's named nix file for each package
-    (conky.override {
-      luaSupport = true;
-      wirelessSupport = true;
-    })
+    conky
     curl
     darcs
     dmenu
@@ -18,9 +10,12 @@
     gnome3.eog
     gnupg
     okular
+    pstree
     rxvt_unicode
     st
+    tmux
     torbrowser
+    tree # pkgs/tools/system/tree
     vim
     wget
     xorg.xev
