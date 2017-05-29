44690c8ac62414b8c240d5119511c699e171e11d
David Asabina <vid@bina.me>
2017-05-29 03:35:38 +0200

    need lmodern.sty

--- doc.nix
+++ doc.nix
@@ -4,6 +4,7 @@
   environment.systemPackages = with pkgs; [
     haskellPackages.pandoc
     haskellPackages.pandoc-citeproc
+    lmodern
     (texlive.combine { inherit (texlive) scheme-basic collection-basic; })
   ];
 }
