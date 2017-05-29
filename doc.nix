c340d3475be1f5e47fc44fa09fe0df2def1fd870
David Asabina <vid@bina.me>
2017-05-29 03:38:34 +0200

    lmodern through texLive aggregator fun

--- doc.nix
+++ doc.nix
@@ -4,7 +4,7 @@
   environment.systemPackages = with pkgs; [
     haskellPackages.pandoc
     haskellPackages.pandoc-citeproc
-    lmodern
+    (texLiveAggregatorFun { paths = [texLive lmodern]; })
     (texlive.combine { inherit (texlive) scheme-basic collection-basic; })
   ];
 }
