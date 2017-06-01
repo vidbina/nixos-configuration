ba5ad174a92de4804585d3c56d07cb7f0eb262df
David Asabina <vid@bina.me>
2017-06-01 17:36:03 +0200

    steam

--- games.nix
+++ games.nix
@@ -1,7 +1,9 @@
 { config, pkgs, ... }:
 
+# https://github.com/NixOS/nixpkgs/commit/192e0c714164a2e537ba3566306c24d9ff888ace#commitcomment-20877324
+# use steam-run for standalone games
 {
   environment.systemPackages = with pkgs; [
-    steam-run
+    steam
   ];
 }
