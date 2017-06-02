9ad4c62682cc63c433ec694d21121d94163da163
David Asabina <vid@bina.me>
2017-06-02 17:08:59 +0200

    opengl 32-bit needed for Steam to work

--- games.nix
+++ games.nix
@@ -3,6 +3,8 @@
 # https://github.com/NixOS/nixpkgs/commit/192e0c714164a2e537ba3566306c24d9ff888ace#commitcomment-20877324
 # use steam-run for standalone games
 {
+  hardware.opengl.driSupport32Bit = true;
+
   environment.systemPackages = with pkgs; [
     steam
   ];
