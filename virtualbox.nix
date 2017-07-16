ec68af3f8ba051a890ba59ba06f719484e9aa67d
David Asabina <vid@bina.me>
2017-07-16 21:21:37 +0200

    virtualbox w/ extension pack

--- virtualbox.nix
+++ virtualbox.nix
@@ -2,9 +2,12 @@
 
 {
   environment.systemPackages = with pkgs; [
-    virtualbox
+    (virtualbox.override {
+      enableExtensionPack = true;
+    })
   ];
 
   virtualisation.virtualbox.host.enable = true;
-}
 
+  nixpkgs.config.virtualbox.enableExtensionPack = true;
+}
