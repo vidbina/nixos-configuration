9a78ac64b362c72f1137a109ddd2c69f443745af
David Asabina <vid@bina.me>
2017-07-26 12:54:44 +0200

    import dev.nix

--- configuration.nix
+++ configuration.nix
@@ -5,42 +5,44 @@
 { config, pkgs, ... }:
 
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-      ./base.nix
-      ./users.nix
+  imports = [
+    # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+    ./base.nix
+    ./users.nix
 
-      ./eid.nix       # eID packages
+    ./eid.nix       # eID packages
 
-      ./packages.nix
-      ./browser.nix
+    ./packages.nix
+    ./browser.nix
 
-      ./audio.nix
-      ./cad.nix       # CAD tools (mostly 3d)
-      ./dev.nix
-      ./doc.nix
-      ./fonts.nix
-      ./docker.nix
-      ./virtualbox.nix
-      ./games.nix
-      ./graphic.nix   # tools for graphics editing and design
-      ./net.nix
-      ./tron.nix      # tools for electronics engineering
-      ./unity3d.nix
+    ./audio.nix
+    ./cad.nix       # CAD tools (mostly 3d)
+    ./dev.nix
+    ./doc.nix
+    ./fonts.nix
+    ./docker.nix
+    ./virtualbox.nix
+    ./games.nix
+    ./graphic.nix   # tools for graphics editing and design
+    ./net.nix
+    ./tron.nix      # tools for electronics engineering
+    ./unity3d.nix
 
-      # TODO: figure out how to make the following configurable
-      # Was thinking about loading a WM env var and then determine which
-      # element to import from that
-      # Looked into:
-      #  - Maybe https://hackage.haskell.org/package/base-4.9.1.0/docs/Data-Maybe.html
-      #  - list comprehensions seem too involved
-      #  - pattern matching
+    # TODO: figure out how to make the following configurable
+    # Was thinking about loading a WM env var and then determine which
+    # element to import from that
+    # Looked into:
+    #  - Maybe https://hackage.haskell.org/package/base-4.9.1.0/docs/Data-Maybe.html
+    #  - list comprehensions seem too involved
+    #  - pattern matching
 
-      ./xmonad.nix
-    #  ./i3.nix
-      ./interfacing.nix
-      ./temp.nix
-      ./urxvt.nix
-    ];
+    ./xmonad.nix
+  #  ./i3.nix
+    ./interfacing.nix
+    ./temp.nix
+    ./urxvt.nix
+
+    ./dev.nix
+  ];
 }
