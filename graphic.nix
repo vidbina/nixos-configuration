965da93e66c413fcfbe33d6497e9e67ff67b2567
David Asabina <vid@bina.me>
2017-08-22 09:10:28 +0200

    trying to use unstable fontforge

--- graphic.nix
+++ graphic.nix
@@ -1,10 +1,16 @@
 { config, pkgs, ... }:
 
+let
+  # http://nixos.org/channels/nixos-unstable/nixexprs.tar.xz
+  unstable = import (
+    fetchTarball
+    https://d3g5gsiof5omrk.cloudfront.net/nixos/unstable/nixos-17.09pre113094.0e5ff82954/nixexprs.tar.xz
+  ) {};
+in
 {
   environment.systemPackages = with pkgs; [
     blender
-    fontforge
-    fontforge-gtk
+    unstable.fontforge-gtk
     gimp
     inkscape
   ];
