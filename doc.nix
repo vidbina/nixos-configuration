253862e74488e6ccf3432504ba9c635e05c3d21f
David Asabina <vid@bina.me>
2017-10-29 16:46:11 +0100

    biber + related LaTeX deps

--- doc.nix
+++ doc.nix
@@ -2,17 +2,21 @@
 
 {
   environment.systemPackages = with pkgs; [
+    biber
     haskellPackages.pandoc
     haskellPackages.pandoc-citeproc
     libreoffice
     (texlive.combine { inherit (texlive)
+      biblatex
       scheme-basic
       collection-basic
       collection-fontsrecommended
       collection-langeuropean
       collection-latexextra
       collection-latexrecommended
+      IEEEtran
       invoice
+      logreq
       realscripts
       xetex
       xetex-def
