eeb1f7128dd41be9061f3df24369a1b8b62f7864
David Asabina <vid@bina.me>
2017-12-20 11:27:26 +0100

    :video_game: ready

--- games.nix
+++ games.nix
@@ -1,9 +1,15 @@
 { config, pkgs, ... }:
 
 {
-  hardware.opengl.driSupport32Bit = true;
-
   environment.systemPackages = with pkgs; [
     steam
   ];
+
+  hardware.opengl.driSupport32Bit = true;
+  hardware.pulseaudio.support32Bit = true;
+
+  services.udev.extraRules = ''
+    SUBSYSTEM=="usb", ATTRS{idVendor}=="28de", MODE="0666"
+    KERNEL=="uinput", MODE="0660", GROUP="users", OPTIONS+="static_node=uinput"
+  '';
 }
