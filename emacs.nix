b6e325367bfc20e036c43d25bb0f9ec20a614801
David Asabina <vid@bina.me>
2018-01-24 15:27:19 +0100

    refactored

--- emacs.nix
+++ emacs.nix
@@ -1,11 +1,12 @@
 { pkgs ? import <nixpkgs> {} }:
 
 let
-  myEmacs = pkgs.emacs;
-  emacsWithPackages = (pkgs.emacsPackagesNgGen myEmacs).emacsWithPackages;
-in emacsWithPackages(epkgs: (with epkgs.melpaStablePackages; [
-  evil
-  monokai-theme 
-]) ++ (with epkgs.melpaPackages; [
-  molokai-theme
-]))
+  current = pkgs.emacs;
+  bundle = (pkgs.emacsPackagesNgGen current).emacsWithPackages;
+  complete = bundle(epkgs: (with epkgs.melpaStablePackages; [
+    evil
+    nix-mode
+  ]) ++ (with epkgs.melpaPackages; [
+    molokai-theme
+  ]));
+in complete
