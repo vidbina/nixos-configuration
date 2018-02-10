47ea110785d45a45ae3d43166533689243a71ffc
David Asabina <vid@bina.me>
2018-02-10 17:09:09 +0100

    relocated interfacing related pkgs
    Anything IBus-related should qualify as input, the placement of D-Bus-related
    artefacts in interfacing.nix is also somewhat sensible since it pertains
    to the desktop environment which covers the UI, hence interfacing.
    
    Perhaps I should consider redesigning the layout for all
    interfacing-related matters. Perhaps I can figure out if all my window
    mananager setups could become part of the interfacing branch of my
    config.

--- interfacing.nix
+++ interfacing.nix
@@ -1,6 +1,20 @@
 { config, pkgs, ... }:
 
 {
+  environment.systemPackages = with pkgs; [
+    dunst              # for displaying IBus Notifications
+    ibus-with-plugins  # for handling input modes (emojis too)
+  ];
+
+  i18n.inputMethod = {
+    enabled = "ibus";
+    ibus = {
+      engines = with pkgs.ibus-engines; [
+        uniemoji
+      ];
+    };
+  };
+
   services.xserver.autoRepeatDelay = 180;
   services.xserver.autoRepeatInterval = 50;
 
@@ -27,5 +41,40 @@
 #   scrollMethod = "twofinger";
     disableWhileTyping = true;
     tapping = false;
+  };
+
+  systemd.user.services.ibus-daemon = {
+    enable = true;
+    #after = ["graphical.target"];
+    wantedBy = [
+      "multi-user.target"
+      "graphical-session.target"
+    ];
+    description = "IBus daemon";
+    script = "${pkgs.ibus-with-plugins}/bin/ibus-daemon";
+    serviceConfig = {
+      Restart = "always";
+      StandardOutput = "syslog";
+    };
+  };
+
+  systemd.user.services.dunst = {
+    enable = true;
+    description  = "Dunst: libnotify client";
+    documentation = [
+      "man:dunst(1)"
+    ];
+    partOf = [
+      "graphical-session.target"
+    ];
+    wantedBy = [
+      "default.target"
+    ];
+
+    serviceConfig = {
+      Type = "dbus";
+      BusName = "org.freedesktop.Notifications";
+      ExecStart = "${pkgs.dunst}/bin/dunst";
+    };
   };
 }
