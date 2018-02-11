c9e527bb39dcafba2b574aab9157fd4a423cfe8f
David Asabina <vid@bina.me>
2018-02-11 15:53:11 +0100

    sort package-related imports

--- configuration.nix
+++ configuration.nix
@@ -8,46 +8,37 @@
   imports = [
     # Include the results of the hardware scan.
     ./hardware-configuration.nix
+
     ./base.nix
+
     ./users.nix
 
-    ./eid.nix       # eID packages
-
     ./packages.nix
-    ./browser.nix
 
     ./audio.nix
-    ./math.nix
+    ./browser.nix
     ./cad.nix       # CAD tools (mostly 3d)
+    ./chat.nix
+    ./dev.nix
     ./dev.nix
     ./doc.nix
-    ./fonts.nix
     ./docker.nix
-    ./virtualbox.nix
+    ./eid.nix       # eID packages
+    ./fonts.nix
     ./games.nix
-    ./media.nix
     ./graphic.nix   # tools for graphics editing and design
-    ./chat.nix
-    ./mail.nix
-    ./productivity.nix
-    ./net.nix
-    ./tron.nix      # tools for electronics engineering
-    ./unity3d.nix
-
-    # TODO: figure out how to make the following configurable
-    # Was thinking about loading a WM env var and then determine which
-    # element to import from that
-    # Looked into:
-    #  - Maybe https://hackage.haskell.org/package/base-4.9.1.0/docs/Data-Maybe.html
-    #  - list comprehensions seem too involved
-    #  - pattern matching
-
-    ./xmonad.nix
-  #  ./i3.nix
+    #./i3.nix
     ./interfacing.nix
+    ./mail.nix
+    ./math.nix
+    ./media.nix
+    ./net.nix
+    ./productivity.nix
     ./temp.nix
     ./terminal.nix
-
-    ./dev.nix
+    ./tron.nix      # tools for electronics engineering
+    ./unity3d.nix
+    ./virtualbox.nix
+    ./xmonad.nix
   ];
 }
