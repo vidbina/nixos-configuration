9f2ffe9eb782696faceccd6a04ee425f06a72883
David Asabina <vid@bina.me>
2018-03-30 08:36:20 +0200

    screenkey from pkgs + unset XMODIFIERS

--- temp.nix
+++ temp.nix
@@ -5,8 +5,13 @@
   # nixpkgs.config.allowBroken = true;
 
   environment.systemPackages = with pkgs; [
+    (screenkey.overrideAttrs(oldAttrs: rec {
+      # https://github.com/rasendubi/nixpkgs/pull/1/files
+      preFixup = ''
+        gappsWrapperArgs+=(--unset XMODIFIERS)
+      '';
+    }))
   ] ++ (with pythonPackages; [
     glances # alternative to htop for in-tree res utilisation and web view
-    screenkey
   ]);
 }
