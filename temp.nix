7f1238c4b6d06ad19c7d3c83701413693ee852ba
David Asabina <vid@bina.me>
2018-07-12 12:03:34 +0200

    fixed screenkey icons + installed slop
    should figure out how to install slop as a dependency of screenkey
    instead of installing it as a separate package.

--- temp.nix
+++ temp.nix
@@ -5,9 +5,12 @@
   # nixpkgs.config.allowBroken = true;
 
   environment.systemPackages = with pkgs; [
+    slop
     (screenkey.overrideAttrs(oldAttrs: rec {
       # https://github.com/rasendubi/nixpkgs/pull/1/files
+      iconPkg = pkgs.tango-icon-theme;
       preFixup = ''
+        $(mkdir -p $out/share/icons/hicolor; cd ${iconPkg}/share/icons/Tango; cp -R . $out/share/icons/hicolor)
         gappsWrapperArgs+=(--unset XMODIFIERS)
       '';
     }))
