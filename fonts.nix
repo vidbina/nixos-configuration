05c9a06864a95ce0eb3e844baab62d975aba7a87
David Asabina <vid@bina.me>
2019-01-17 16:58:54 +0100

    Goofing around with emoji fonts

--- fonts.nix
+++ fonts.nix
@@ -5,9 +5,16 @@
     enableFontDir = true;
     enableGhostscriptFonts = true;
     fonts = with pkgs; [
+      emojione
       font-awesome-ttf
       noto-fonts-emoji
       powerline-fonts
+    ];
+    fontconfig.defaultFonts.monospace = [
+      "DejaVu Sans Mono"
+      "Noto Emoji"
+      "EmojiOne Color"
+      "Noto Color Emoji"
     ];
   };
 }
