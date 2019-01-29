f34c9ffc9b5c2c5249294b4203602b032f0ce727
David Asabina <vid@bina.me>
2019-01-29 20:00:55 +0100

    Add GanttProject 2.8.9

--- productivity.nix
+++ productivity.nix
@@ -6,6 +6,41 @@
     tasksh
     taskwarrior
     timewarrior
+    (ganttproject-bin.overrideAttrs(oldAttrs: rec {
+      version = "2.8.9";
+      name = "ganttproject-bin-${version}";
+      src = fetchzip {
+        sha256 = "1fmfrsy9z2nff0bxwj7xsfbwkb9y1dmssvy5wkmf9ngihyzj3w1k";
+        url = "https://dl.ganttproject.biz/ganttproject-${version}/ganttproject-${version}-r2335.zip";
+      };
+      installPhase = let
+
+        desktopItem = makeDesktopItem {
+          name = "ganttproject";
+          exec = "ganttproject";
+          icon = "ganttproject";
+          desktopName = "GanttProject";
+          genericName = "Shedule and manage projects";
+          comment = oldAttrs.meta.description;
+          categories = "Office;Application;";
+        };
+
+      in ''
+        mkdir -pv "$out/share/ganttproject"
+        cp -rv *  "$out/share/ganttproject"
+
+        mkdir -pv "$out/bin"
+        wrapProgram "$out/share/ganttproject/ganttproject" \
+          --set JAVA_HOME "${jre}" \
+          --set _JAVA_OPTIONS "-Dawt.useSystemAAFontSettings=on"
+        mv -v "$out/share/ganttproject/ganttproject" "$out/bin"
+
+        #install -v -Dm644 \
+        #  plugins/net.sourceforge.ganttproject/data/resources/icons/ganttproject.png \
+        #  "$out/share/pixmaps/ganttproject.png"
+        cp -rv "${desktopItem}/share/applications" "$out/share"
+      '';
+    }))
     vym
   ];
 }
