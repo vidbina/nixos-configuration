7d27e5be59ac4adeba99479dd870b862d3b332f4
David Asabina <vid@bina.me>
2019-11-29 11:49:40 +0100

    Remove deleted telegram-cli package

--- chat.nix
+++ chat.nix
@@ -2,7 +2,6 @@
 
 {
   environment.systemPackages = with pkgs; [
-    telegram-cli
     (tdesktop.overrideAttrs(oldAttrs: rec {
       nativeBuildInputs = oldAttrs.nativeBuildInputs ++ [
         wrapGAppsHook
