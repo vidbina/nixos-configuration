f26e090e5ee28ca49b343c46626dd892309f1896
David Asabina <vid@bina.me>
2020-03-02 18:37:39 -0600

    Sort dev pkgs

--- dev.nix
+++ dev.nix
@@ -2,12 +2,15 @@
 
 {
   environment.systemPackages = with pkgs; [
+    # deno # WIP
+    #htmlTidy
+    #mitscheme
+    #stdenv
     (import ./emacs.nix { inherit pkgs; })
     asciinema
     bazaar
     cmakeCurses
     darcs
-    # deno # WIP
     gdb
     ghc
     git
@@ -17,11 +20,8 @@
     go
     haskellPackages.hlint
     httpie
-    #htmlTidy
     jq
-    #mitscheme
     neovim
-    #stdenv
     sqlite_3_30_1
     subversion
     xxd
