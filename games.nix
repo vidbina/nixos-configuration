0d8a9dab4ff06d44d85ba6ccdb85d24c84ed86f6
David Asabina <vid@bina.me>
2020-03-21 21:32:15 +0100

    Install Steam

--- games.nix
+++ games.nix
@@ -5,5 +5,13 @@
     anki
     godot
     steam
+    steam-run
   ];
+
+  hardware.opengl = {
+    driSupport32Bit = true;
+    extraPackages32 = with pkgs.pkgsi686Linux; [ libva ];
+  };
+
+  hardware.pulseaudio.support32Bit = true;
 }
