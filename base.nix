c040942771fc523ce0f5cd43339cf89e5ec725cf
David Asabina <vid@bina.me>
2020-11-07 23:29:11 +0100

    feat: Configure v4l2loopback mod to /dev/video10

--- base.nix
+++ base.nix
@@ -24,12 +24,13 @@ in
       "virtio_ring"
       "virtio_net"
       "vboxguest"
+      "v4l2loopback"
     ];
     extraModulePackages = [
       config.boot.kernelPackages.v4l2loopback
     ];
     extraModprobeConfig = ''
-      options v4l2loopback exclusive_caps=1 video_nr=1 card_label="obs"
+      options v4l2loopback exclusive_caps=1 video_nr=10 card_label="v4l2-cam"
     '';
     loader = {
       systemd-boot.enable = true;
