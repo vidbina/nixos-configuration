ad92ac6ad8fd1b9477731d45aa0d5e2a197469e4
David Asabina <vid@bina.me>
2020-11-23 09:30:49 +0100

    feat: Add wee-slack and autosort to weechat

--- chat.nix
+++ chat.nix
@@ -3,7 +3,20 @@
 {
   environment.systemPackages = with pkgs; [
     tdesktop
-    weechat
+    (
+      weechat.override {
+        # https://nixos.org/manual/nixpkgs/stable/#sec-weechat
+        configure = { availablePlugins, ... }: {
+          plugins = with availablePlugins; [
+            python
+          ];
+          scripts = with weechatScripts; [
+            wee-slack
+            weechat-autosort
+          ];
+        };
+      }
+    )
     zoom-us
   ];
 }
