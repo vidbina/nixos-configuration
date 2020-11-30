4867c2bfd7d246a03a2cf8d60ad0dae13dc0709b
David Asabina <vid@bina.me>
2020-11-30 17:39:52 +0100

    feat: Build checkmake

--- dev.nix
+++ dev.nix
@@ -9,6 +9,47 @@
     (import ./emacs.nix { inherit pkgs; })
     asciinema
     bazaar
+    (
+      buildGoPackage rec {
+        pname = "checkmake";
+        version = "2020-11-30";
+
+        goPackagePath = "github.com/mrtazz/checkmake";
+
+        nativeBuildInputs = [ makeWrapper ];
+
+        src = fetchFromGitHub {
+          owner = "mrtazz";
+          repo = pname;
+          rev = "575315c9924da41534a9d0ce91c3f0d19bb53ffc";
+          sha256 = "121rsl9mh3wwadgf8ggi2xnb050pak6ma68b2sw5j8clmxbrqli3";
+        };
+
+        wrapperPath = stdenv.lib.makeBinPath (
+          [
+            pandoc
+          ]
+        );
+
+        postFixup = ''
+          wrapProgram $bin/bin/checkmake \
+            --prefix PATH : "${wrapperPath}"
+        '';
+
+        meta = with stdenv.lib; {
+          description = "Experimental tool for linting and checking Makefiles";
+          homepage = https://github.com/mrtazz/checkmake;
+          license = licenses.mit;
+          maintainers = with maintainers; [ vidbina ];
+          platforms = platforms.linux;
+
+          longDescription = ''
+            checkmake is an experimental tool for linting and checking
+            Makefiles. It may not do what you want it to.
+          '';
+        };
+      }
+    )
     cmakeCurses
     darcs
     gdb
