6733fb8cb89ba3652141ac2c355aea6889b56677
David Asabina <vid@bina.me>
2020-12-04 09:30:27 +0100

    chore: Disable checkmake (build error)

--- dev.nix
+++ dev.nix
@@ -8,47 +8,52 @@
     #stdenv
     (import ./emacs.nix { inherit pkgs; })
     asciinema
-    (
-      buildGoPackage rec {
-        pname = "checkmake";
-        version = "2020-11-30";
+    #(
+    #  buildGoPackage rec {
+    #    pname = "checkmake";
+    #    version = "2020-11-30";
 
-        goPackagePath = "github.com/mrtazz/checkmake";
+    #    goPackagePath = "github.com/mrtazz/checkmake";
 
-        nativeBuildInputs = [ makeWrapper ];
+    #    nativeBuildInputs = [ tree makeWrapper ];
 
-        src = fetchFromGitHub {
-          owner = "mrtazz";
-          repo = pname;
-          rev = "575315c9924da41534a9d0ce91c3f0d19bb53ffc";
-          sha256 = "121rsl9mh3wwadgf8ggi2xnb050pak6ma68b2sw5j8clmxbrqli3";
-        };
+    #    src = fetchFromGitHub {
+    #      owner = "mrtazz";
+    #      repo = pname;
+    #      rev = "575315c9924da41534a9d0ce91c3f0d19bb53ffc";
+    #      sha256 = "121rsl9mh3wwadgf8ggi2xnb050pak6ma68b2sw5j8clmxbrqli3";
+    #    };
 
-        wrapperPath = stdenv.lib.makeBinPath (
-          [
-            pandoc
-          ]
-        );
+    #    buildInputs = [ pandoc ];
+    #    #wrapperPath = stdenv.lib.makeBinPath (
+    #    #  [
+    #    #    pandoc
+    #    #  ]
+    #    #);
 
-        postFixup = ''
-          wrapProgram $bin/bin/checkmake \
-            --prefix PATH : "${wrapperPath}"
-        '';
+    #    #preFixup = ''
+    #    #  pwd
+    #    #  tree go
+    #    #'';
+    #    #postFixup = ''
+    #    #  wrapProgram $bin/bin/checkmake \
+    #    #    --prefix PATH : "${wrapperPath}"
+    #    #'';
 
-        meta = with stdenv.lib; {
-          description = "Experimental tool for linting and checking Makefiles";
-          homepage = https://github.com/mrtazz/checkmake;
-          license = licenses.mit;
-          maintainers = with maintainers; [ vidbina ];
-          platforms = platforms.linux;
+    #    meta = with stdenv.lib; {
+    #      description = "Experimental tool for linting and checking Makefiles";
+    #      homepage = https://github.com/mrtazz/checkmake;
+    #      license = licenses.mit;
+    #      maintainers = with maintainers; [ vidbina ];
+    #      platforms = platforms.linux;
 
-          longDescription = ''
-            checkmake is an experimental tool for linting and checking
-            Makefiles. It may not do what you want it to.
-          '';
-        };
-      }
-    )
+    #      longDescription = ''
+    #        checkmake is an experimental tool for linting and checking
+    #        Makefiles. It may not do what you want it to.
+    #      '';
+    #    };
+    #  }
+    #)
     cmakeCurses
     darcs
     gdb
