6181225cd82356cda523ea2b91b59bcd8766cc97
David Asabina <vid@bina.me>
2021-01-19 13:01:52 +0100

    feat: Install checkmake (based on PR 109950)
    https://github.com/NixOS/nixpkgs/pull/109950

--- dev.nix
+++ dev.nix
@@ -8,52 +8,56 @@
     #stdenv
     (import ./emacs.nix { inherit pkgs; })
     asciinema
-    #(
-    #  buildGoPackage rec {
-    #    pname = "checkmake";
-    #    version = "2020-11-30";
+    #checkmake
+    (
+      buildGoPackage rec {
+        pname = "checkmake";
+        version = "0.1.0-2020.11.30";
 
-    #    goPackagePath = "github.com/mrtazz/checkmake";
+        goPackagePath = "github.com/mrtazz/checkmake";
 
-    #    nativeBuildInputs = [ tree makeWrapper ];
+        src = fetchFromGitHub {
+          owner = "mrtazz";
+          repo = pname;
+          rev = "575315c9924da41534a9d0ce91c3f0d19bb53ffc";
+          sha256 = "121rsl9mh3wwadgf8ggi2xnb050pak6ma68b2sw5j8clmxbrqli3";
+        };
 
-    #    src = fetchFromGitHub {
-    #      owner = "mrtazz";
-    #      repo = pname;
-    #      rev = "575315c9924da41534a9d0ce91c3f0d19bb53ffc";
-    #      sha256 = "121rsl9mh3wwadgf8ggi2xnb050pak6ma68b2sw5j8clmxbrqli3";
-    #    };
+        nativeBuildInputs = [ pandoc ];
 
-    #    buildInputs = [ pandoc ];
-    #    #wrapperPath = stdenv.lib.makeBinPath (
-    #    #  [
-    #    #    pandoc
-    #    #  ]
-    #    #);
+        preBuild =
+          let
+            buildVars = {
+              version = version;
+              buildTime = "N/A";
+              builder = "nix";
+              goversion = "$(go version | egrep -o 'go[0-9]+[.][^ ]*')";
+            };
+            buildVarsFlags = lib.concatStringsSep " " (lib.mapAttrsToList (k: v: "-X main.${k}=${v}") buildVars);
+          in
+          ''
+            buildFlagsArray+=("-ldflags=${buildVarsFlags}")
+          '';
 
-    #    #preFixup = ''
-    #    #  pwd
-    #    #  tree go
-    #    #'';
-    #    #postFixup = ''
-    #    #  wrapProgram $bin/bin/checkmake \
-    #    #    --prefix PATH : "${wrapperPath}"
-    #    #'';
+        postInstall = ''
+          pandoc -s -t man -o checkmake.1 go/src/${goPackagePath}/man/man1/checkmake.1.md
+          mkdir -p $out/share/man/man1
+          mv checkmake.1 $out/share/man/man1/checkmake.1
+        '';
 
-    #    meta = with stdenv.lib; {
-    #      description = "Experimental tool for linting and checking Makefiles";
-    #      homepage = https://github.com/mrtazz/checkmake;
-    #      license = licenses.mit;
-    #      maintainers = with maintainers; [ vidbina ];
-    #      platforms = platforms.linux;
-
-    #      longDescription = ''
-    #        checkmake is an experimental tool for linting and checking
-    #        Makefiles. It may not do what you want it to.
-    #      '';
-    #    };
-    #  }
-    #)
+        meta = with lib; {
+          description = "Experimental tool for linting and checking Makefiles";
+          homepage = https://github.com/mrtazz/checkmake;
+          license = licenses.mit;
+          maintainers = with maintainers; [ vidbina ];
+          platforms = platforms.linux;
+          longDescription = ''
+            checkmake is an experimental tool for linting and checking
+            Makefiles. It may not do what you want it to.
+          '';
+        };
+      }
+    )
     cmakeCurses
     darcs
     gdb
