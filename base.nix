b3f7255d0988e738dd31a85424c6332c2ff60953
David Asabina <vid@bina.me>
2021-02-04 13:50:24 +0100

    chore: Disable brightness keybinding override

--- base.nix
+++ base.nix
@@ -90,8 +90,8 @@ in
     actkbd = {
       enable = true;
       bindings = [
-        { keys = [ 224 ]; events = [ "key" ]; command = "/run/current-system/sw/bin/light -A 10"; }
-        { keys = [ 225 ]; events = [ "key" ]; command = "/run/current-system/sw/bin/light -U 10"; }
+        # { keys = [ 224 ]; events = [ "key" ]; command = "/run/current-system/sw/bin/light -A 10"; }
+        # { keys = [ 225 ]; events = [ "key" ]; command = "/run/current-system/sw/bin/light -U 10"; }
       ];
     };
 
