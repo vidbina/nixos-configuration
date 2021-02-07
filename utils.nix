330fcb9512fc9cad98b128d71e32b6cd48c48980
David Asabina <vid@bina.me>
2021-02-07 17:29:24 +0100

    refactor: Move commented jsom override to let

--- utils.nix
+++ utils.nix
@@ -1,5 +1,21 @@
 { config, pkgs, ... }:
 let
+  #josm-vidbina = with pkgs; josm.overrideAttrs (oldAttrs: rec {
+  #  buildCommand = ''
+  #    mkdir -p $out/bin $out/share/java
+  #    cp -v $src $out/share/java/josm.jar
+
+  #    makeWrapper ${jdk11}/bin/java $out/bin/josm \
+  #      --add-flags "-Dsun.java2d.uiScale=1.3" \
+  #      --add-flags "-jar $out/share/java/josm.jar"
+
+  #    mkdir -p $out/share/applications
+  #    cp $desktopItem/share/applications"/"* $out/share/applications
+  #    mkdir -p $out/share/pixmaps
+  #    ${unzip}/bin/unzip -p $src images/logo_48x48x32.png > $out/share/pixmaps/josm.png
+  #  '';
+  #});
+
   pass-vidbina = with pkgs; pass.withExtensions (exts: [ exts.pass-otp ]);
 in
 {
@@ -28,25 +44,6 @@ in
     indent
     iotop
     jmtpfs
-    #(
-    #  josm.overrideAttrs (
-    #    oldAttrs: rec {
-    #      buildCommand = ''
-    #        mkdir -p $out/bin $out/share/java
-    #        cp -v $src $out/share/java/josm.jar
-
-    #        makeWrapper ${jdk11}/bin/java $out/bin/josm \
-    #          --add-flags "-Dsun.java2d.uiScale=1.3" \
-    #          --add-flags "-jar $out/share/java/josm.jar"
-
-    #        mkdir -p $out/share/applications
-    #        cp $desktopItem/share/applications"/"* $out/share/applications
-    #        mkdir -p $out/share/pixmaps
-    #        ${unzip}/bin/unzip -p $src images/logo_48x48x32.png > $out/share/pixmaps/josm.png
-    #      '';
-    #    }
-    #  )
-    #)
     libnotify
     mc
     networkmanagerapplet
