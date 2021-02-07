f89701ee8c139c689805c318e4dd7c6ebfce2316
David Asabina <vid@bina.me>
2021-02-07 17:23:52 +0100

    feat: Install texlive distribution (for pandoc)
    For pandoc, pdflatex is needed and this texlive configuration installs
    all the basics for basic pandoc markdown-to-pdf conversion to work.

--- doc.nix
+++ doc.nix
@@ -10,6 +10,45 @@ let
       sha256 = "sha256:018z06bfcw0l4k2zdwbgxna9fss4wdqj64ckw5qjis14sb3zkr28";
     };
   });
+  texlive-asabina = with pkgs; (texlive.combine {
+    inherit (texlive)
+      scheme-basic
+      luatex
+
+      IEEEtran
+      background
+      bashful
+      collection-basic
+      collection-fontsrecommended
+      collection-langeuropean
+      collection-langgerman
+      collection-latexrecommended
+      datetime
+      draftwatermark
+      enumitem
+      eso-pic
+      etoolbox
+      everypage
+      fmtcount
+      lastpage
+      latexdiff
+      mdframed
+      needspace
+      numprint
+      paracol
+      pdfcrop
+      pgfgantt
+      soul
+      tableof
+      titlepic
+      tocloft
+      ulem
+      xargs
+      xetex
+      xstring
+      xtab
+      ;
+  });
 in
 {
   environment.systemPackages = with pkgs; [
@@ -25,6 +64,7 @@ in
     libreoffice
     okular
     pandoc-plantuml-filter
+    texlive-asabina
     pdftk
     scim
     visidata-vidbina
