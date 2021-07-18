52a69e1bb352d06379915fbf4e3d0417d196cd84
David Asabina <vid@bina.me>
2021-07-18 14:00:28 +0200

    chore: Uninstall qutebrowser

--- browser.nix
+++ browser.nix
@@ -3,16 +3,6 @@
 {
   environment.systemPackages = with pkgs; [
     firefox
-    qutebrowser
-    # (
-    #   qutebrowser.overrideAttrs (
-    #     oldAttrs: rec {
-    #       propagatedBuildInputs = [ oldAttrs.propagatedBuildInputs ] /*++ (
-    #         with python3Packages; [ pyflakes pycodestyle ]
-    #       )*/;
-    #     }
-    #   )
-    # )
   ];
 
   nixpkgs.config.firefox = {
