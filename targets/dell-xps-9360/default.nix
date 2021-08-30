777f17aafda865f8397ba4624446273c3bfe6b7c
David Asabina <vid@bina.me>
2021-08-30 13:29:49 +0200

    refactor: Reimport modules for Dell XPS 9360
    instead of importing ../../configuration.nix in order to provide a bit
    more control in customizing target configs.

--- targets/dell-xps-9360/default.nix
+++ targets/dell-xps-9360/default.nix
@@ -1,7 +1,42 @@
 { nixpkgs }: nixpkgs.lib.nixosSystem {
   system = "x86_64-linux";
   modules = [
-    ../../configuration.nix
     ./hardware-configuration.nix
+
+    ../../base.nix
+
+    ../../users.nix
+
+    ../../utils.nix
+
+    # basics
+    ../../dev.nix
+    ../../emacs.nix
+    ../../fonts.nix
+    ../../interfacing.nix
+    ../../terminal.nix
+    ../../xmonad.nix
+    ../../net.nix
+
+    # other
+    ../../audio.nix
+    ../../browser.nix
+    ../../cad.nix # CAD tools (mostly 3d)
+    ../../chat.nix
+    ../../crypto.nix
+    ../../doc.nix
+    #../../eid.nix       # eID packages
+    ../../games.nix
+    ../../graphic.nix # tools for graphics editing and design
+    ##../../i3.nix
+    ../../mail.nix
+    ../../math.nix
+    ../../media.nix
+    ../../productivity.nix
+    ../../sec.nix
+    #../../temp.nix
+    ../../tron.nix # tools for electronics engineering
+    #../../unity3d.nix
+    ../../virt.nix
   ];
 }
