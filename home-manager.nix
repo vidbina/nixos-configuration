96262ae8652439228f89994e51f43f2be5dfc2c6
David Asabina <vid@bina.me>
2021-08-31 15:14:04 +0200

    doc: Add home-manager manual

--- home-manager.nix
+++ home-manager.nix
@@ -4,8 +4,17 @@
     useUserPackages = true;
 
     users.vidbina = {
+      # https://nix-community.github.io/home-manager/options.html
       programs = {
         home-manager.enable = true;
+      };
+
+      manual = {
+        # Use `home-manager-help`
+        html.enable = true;
+
+        # Use `man home-configuration.nix`
+        manpages.enable = true;
       };
     };
   };
