3e21cb549227924a3a98dbfc25cd7294412a06a8
David Asabina <vid@bina.me>
2021-09-06 00:56:49 +0200

    feat: Manage neovim with nix

--- vim.nix
+++ vim.nix
@@ -1,34 +1,34 @@
 { pkgs, ... }:
 {
-  environment.systemPackages = with pkgs; [
-    (neovim.override {
-      configure = {
-        packages.myVimPackage = {
-          start = with vimPlugins; [
-            gitgutter
-            tagbar
-            nerdtree
+  programs.neovim = {
+    enable = true;
+    configure = {
+      packages.myVimPackage = {
+        start = with pkgs.vimPlugins; [
+          gitgutter
+          tagbar
+          nerdtree
 
-            # async syntax checker w/ LSP support
-            # https://github.com/dense-analysis/ale
-            ale
+          # async syntax checker w/ LSP support
+          # https://github.com/dense-analysis/ale
+          ale
 
-            # languages
-            vim-nix
-            ## doc
-            plantuml-syntax
-            vim-markdown
-            ## infra
-            vim-terraform
-            ## web
-            elm-syntax
-            vim-javascript
-            vim-jsx-pretty
-            typescript-vim
-          ];
-        };
+          # languages
+          vim-nix
+          ## doc
+          plantuml-syntax
+          vim-markdown
+          ## infra
+          vim-terraform
+          ## web
+          vim-javascript
+          vim-jsx-pretty
+          typescript-vim
+        ];
       };
-      viAlias = true;
-    })
-  ];
+    };
+    viAlias = true;
+    vimAlias = true;
+    withRuby = true;
+  };
 }
