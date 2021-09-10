0908d29cdc7218fbf9db9df6ad2add9eabf17689
David Asabina <vid@bina.me>
2021-09-10 23:42:44 +0200

    chore: Stub user urxvt config

--- home-configuration.nix
+++ home-configuration.nix
@@ -1,6 +1,3 @@
-#{ vidbina-xmobar-config
-#, username
-#}:
 { lib, config, options, modulesPath, specialArgs, pkgs }: {
   config.home-manager = {
     useGlobalPkgs = true;
@@ -10,6 +7,78 @@
       # man home-configuration.nix
       programs = {
         home-manager.enable = true;
+
+        urxvt =
+          let
+            my-rxvt-unicode = pkgs.rxvt-unicode.override {
+              configure = { availablePlugins, ... }: {
+                plugins = (builtins.attrValues availablePlugins) ++ [
+                  (pkgs.writeTextFile rec {
+                    name = "urxvt-colors";
+                    text = ''
+                      #! perl
+                      sub on_user_command {
+                        my ($self, $cmd) = @_;
+                        my $output = `dynamic-colors cycle`;
+                        $self->cmd_parse($output);
+                      }
+                    '';
+                    destination = "/lib/urxvt/perl/${name}";
+                    executable = true;
+                  })
+                ];
+              };
+            };
+          in
+          {
+            enable = true;
+            package = my-rxvt-unicode;
+            iso14755 = false;
+            extraConfig = {
+              "perl-lib" = "${my-rxvt-unicode}/lib/urxvt/perl";
+              "perl-ext-common" = builtins.concatStringsSep "," [
+                "default"
+                "font-size"
+                "url-select"
+                "urxvt-colors"
+              ];
+              "url-select.autocopy" = true;
+              # TODO: Define launcher using ${xsel-copy-url}/bin/xsel-copy-url
+              "url-select.launcher" = "xsel-copy-url";
+              "url-select.underline" = true;
+              # See `man urxvt` for guidance on the colors
+              "background" = "#000000";
+              "foreground" = "#ffffff";
+              "color0" = "#000000"; # black, Black
+              "color1" = "#ff0000"; # red, Red3
+              "color2" = "#55ff55"; # green, Green3
+              "color3" = "#ffd42a"; # yellow, Yellow3
+              "color4" = "#2a7fff"; # blue, Blue3
+              "color5" = "#dd55ff"; # magenta, Magenta3
+              "color6" = "#00aad4"; # cyan, Cyan3
+              "color7" = "#cccccc"; # white, AntiqueWhite
+              "color8" = "#333333"; # bright black, Grey25
+              "color9" = "#ff0066"; # bright red, Red
+              "color10" = "#00ff00"; # bright green, Green
+              "color11" = "#ff6600"; # bright yellow, Yellow
+              "color12" = "#00b3ff"; # bright blue, Blue
+              "color13" = "#ff2ad4"; # bright magenta, Magenta
+              "color14" = "#00ffcc"; # bright cyan, Cyan
+              "color15" = "#ffffff"; # bright white, White
+            };
+            fonts = [
+              "xft:DejaVu Sans Mono:pixelsize=12:antialias=true"
+              "xft:Fira Code:size=12:antialias=true"
+              "xft:Iosevka:size=12"
+            ];
+            keybindings = {
+              "C-minus" = "perl:font-size:decrease";
+              "C-plus" = "perl:font-size:increase";
+              "C-=" = "perl:font-size:reset";
+              "M-u" = "perl:url-select:select_next";
+              "F12" = "perl:urxvt-colors:";
+            };
+          };
       };
 
       manual = {
