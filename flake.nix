458f09dc77b5f0ff46702dbac9e030faf3e77554
David Asabina <vid@bina.me>
2021-11-05 18:38:13 +0100

    refactor: Define general helper for system def

--- flake.nix
+++ flake.nix
@@ -28,35 +28,40 @@
     , home-manager
     , emacs-overlay
     } @ args:
+    let
+      # TODO: Use flake-utils to do this well
+      mkLinuxSystem = { target, module }: nixpkgs.lib.nixosSystem {
+        #inherit system;
+        system = "x86_64-linux";
+        modules = [
+          #nixos-hardware.nixosModules."${module}"
+          ./base.nix
+          (./. + "/targets/${target}/hardware-configuration.nix")
+          (./. + "/targets/${target}/custom.nix")
+
+
+          home-manager.nixosModules.home-manager
+
+          # Infuse config/dotfile flakes
+          # NOTE: Define after importing users.nix (because of my-config dep)
+          ({ config, lib, ... }: {
+            config.home-manager = {
+              users = (lib.genAttrs [ config.my-config.handle ] (username: {
+                home = { };
+              }));
+            };
+
+            config.nixpkgs.overlays = [
+              (import emacs-overlay)
+            ];
+          })
+        ];
+      };
+    in
     {
       nixosConfigurations = {
-        dell-xps-9360 = nixpkgs.lib.nixosSystem {
-          #inherit system;
-          system = "x86_64-linux";
-          modules = [
-            nixos-hardware.nixosModules.dell-xps-13-9360
-            ./targets/dell-xps-9360/hardware-configuration.nix
-
-            ./base.nix
-
-            home-manager.nixosModules.home-manager
-
-            # Infuse config/dotfile flakes
-            # NOTE: Define after importing users.nix (because of my-config dep)
-            ({ config, lib, ... }: {
-              config.home-manager = {
-                users = (lib.genAttrs [ config.my-config.handle ] (username: {
-                  home = { };
-                }));
-              };
-
-              config.nixpkgs.overlays = [
-                (import emacs-overlay)
-              ];
-            })
-          ];
-
-        };
+        dell-xps-9360 = mkLinuxSystem { target = "dell-xps-9360"; module = "dell-xps-13-9360"; };
+        dell-precision-5560 = mkLinuxSystem { target = "dell-precision-5560"; module = "dell-precision-5530"; };
       };
     };
 }
