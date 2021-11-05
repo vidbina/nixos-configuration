6d24aff8dbd2496fe7b69d7806d39009bb7e131c
David Asabina <vid@bina.me>
2021-11-05 13:10:09 +0100

    chore: Use emacs-overlay

--- flake.nix
+++ flake.nix
@@ -15,6 +15,10 @@
       url = github:nix-community/home-manager/release-21.05;
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    emacs-overlay = {
+      url = github:nix-community/emacs-overlay;
+    };
   };
 
   outputs =
@@ -22,6 +26,7 @@
     , nixpkgs
     , nixos-hardware
     , home-manager
+    , emacs-overlay
     } @ args:
     {
       nixosConfigurations = {
@@ -41,10 +46,13 @@
             ({ config, lib, ... }: {
               config.home-manager = {
                 users = (lib.genAttrs [ config.my-config.handle ] (username: {
-                  home = {
-                  };
+                  home = { };
                 }));
               };
+
+              config.nixpkgs.overlays = [
+                (import emacs-overlay)
+              ];
             })
           ];
 
