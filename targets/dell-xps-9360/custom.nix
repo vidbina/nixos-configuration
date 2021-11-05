40d62f217d34e63d5261adeef34127bdc9b31e33
David Asabina <vid@bina.me>
2021-11-05 18:12:52 +0100

    chore: Move mech-specifics out of gen base.nix

---
+++ targets/dell-xps-9360/custom.nix
@@ -0,0 +1,63 @@
+{ config, ... }:
+
+{
+  # Use the systemd-boot EFI boot loader.
+  boot = {
+    cleanTmpDir = true;
+    #tmpOnTmpfs = true;
+
+    kernelModules = [
+      "af_packet"
+      "v4l2loopback"
+    ];
+    extraModulePackages = [
+      config.boot.kernelPackages.v4l2loopback
+    ];
+    extraModprobeConfig = ''
+      options v4l2loopback exclusive_caps=1 video_nr=10 card_label="v4l2-cam"
+    '';
+    loader = {
+      systemd-boot.enable = true;
+      efi.canTouchEfiVariables = true;
+      grub.device = "/dev/nvme0n1";
+    };
+    initrd.luks.devices = {
+      "base.crypt.small" = {
+        device = "/dev/nvme0n1p3"; # 100 GiB
+        preLVM = true;
+      };
+      "store" = {
+        device = "/dev/nvme0n1p5"; # 300 GiB
+        preLVM = true;
+      };
+    };
+  };
+
+  hardware = {
+    cpu.intel.updateMicrocode = true;
+
+    opengl = {
+      enable = true;
+      extraPackages = with pkgs; [
+        intel-media-driver
+      ];
+    };
+  };
+
+  fileSystems."/home" = {
+    device = "/dev/mapper/store-store";
+    fsType = "btrfs";
+  };
+
+  fileSystems."/nix" = {
+    device = "/dev/nvme0n1p6";
+    fsType = "btrfs";
+    neededForBoot = true;
+    options = [ "noatime" ];
+  };
+
+  #  fileSystems."/home" = {
+  #    device = "/dev/disk/by-label/store";
+  #    fsType = "btrfs";
+  #  };
+}
