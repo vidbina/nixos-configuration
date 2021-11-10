e8d87fbb6144228ed58663a0c516301d92679aa6
David Asabina <vid@bina.me>
2021-11-10 18:38:49 +0100

    feat: Install personal hunspell dict with manuals

--- doc.nix
+++ doc.nix
@@ -71,6 +71,7 @@ in
     biber
     evince
     ghostscript
+    hunspell-personal
     libreoffice
     okular
     texlive-asabina
@@ -92,4 +93,57 @@ in
       wiktionary
     ];
   };
+
+  nixpkgs.overlays = [
+    (final: prev: rec {
+      hunspell-personal =
+        with prev;
+        let
+          personal-langs = with hunspellDicts; [
+            de_DE
+            en_US-large
+            (stdenv.mkDerivation rec {
+              pname = "hunspell-dict-nl-opentaal";
+              version = "2.20.19";
+              src = fetchFromGitHub {
+                owner = "OpenTaal";
+                repo = "opentaal-hunspell";
+                rev = "${version}";
+                sha256 = "sha256-/rYufNGkXWH7UQDa8ZI55JtP9LM+0j7Pad8zm2tFqko=";
+              };
+              meta = {
+                description = "Dutch dictionary for spelling checker Hunspell and Nuspell";
+                homepage = "https://github.com/OpenTaal/opentaal-hunspell";
+                license = {
+                  fullName = "Revised BSD License and/or CC BY 3.0";
+                  url = "https://github.com/OpenTaal/opentaal-hunspell/blob/${version}/LICENSE.txt";
+                };
+                maintainers = with maintainers; [ ];
+                platforms = lib.platforms.all;
+              };
+
+              dictFileName = "nl";
+              installPhase = ''
+                # hunspell dicts
+                install -dm755 "$out/share/hunspell"
+                install -m644 ${dictFileName}.dic "$out/share/hunspell/"
+                install -m644 ${dictFileName}.aff "$out/share/hunspell/"
+
+                # myspell dicts symlinks
+                install -dm755 "$out/share/myspell/dicts"
+                ln -sv "$out/share/hunspell/${dictFileName}.dic" "$out/share/myspell/dicts/"
+                ln -sv "$out/share/hunspell/${dictFileName}.aff" "$out/share/myspell/dicts/"
+                runHook postInstall
+              '';
+            })
+          ];
+        in
+        (hunspellWithDicts personal-langs).overrideAttrs (old: {
+          buildCommand = ''
+            ${old.buildCommand}
+            cp -r ${hunspell.man}/share $out/share
+          '';
+        });
+    })
+  ];
 }
