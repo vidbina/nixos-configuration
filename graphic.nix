9933eee99f5bfe4b8f6fcd9cd90c9a030f69fd60
David Asabina <vid@bina.me>
2021-11-17 09:40:31 +0100

    chore: Cleanup on fontforge remnant

--- graphic.nix
+++ graphic.nix
@@ -1,18 +1,4 @@
 { config, pkgs, ... }:
-let
-  # /nixpkgs points to a local clone of the nixpkgs repository
-  fontforge-dev = pkgs.callPackage /nixpkgs/pkgs/tools/misc/fontforge {
-    withSpiro = true;
-    withGTK = true;
-  };
-  # http://nixos.org/channels/nixos-unstable/nixexprs.tar.xz
-  unstable = import
-    (
-      fetchTarball
-        https://d3g5gsiof5omrk.cloudfront.net/nixos/unstable/nixos-17.09pre113094.0e5ff82954/nixexprs.tar.xz
-    )
-    { };
-in
 {
   environment.systemPackages = with pkgs; [
     aseprite-unfree
