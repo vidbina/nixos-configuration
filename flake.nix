c80256c52c700c1d1d5305601da778386622a4d7
David Asabina <vid@bina.me>
2022-04-24 19:49:17 +0000

    refactor: format flake.nix for brevity

--- flake.nix
+++ flake.nix
@@ -41,15 +41,16 @@
         ];
       };
       targets = [ "dell-xps-9360" "dell-precision-5560" ];
-      hardwareModules = {
-        dell-xps-9360 = nixos-hardware.nixosModules.dell-xps-13-9360;
-        dell-precision-5560 = nixos-hardware.nixosModules.dell-precision-5530;
+      hardwareModules = with nixos-hardware.nixosModules; {
+        dell-xps-9360 = dell-xps-13-9360;
+        dell-precision-5560 = dell-precision-5530;
       };
     in
     {
-      nixosConfigurations = (nixpkgs.lib.genAttrs targets (target: mkLinuxSystem {
-        inherit target;
-        module = hardwareModules."${target}";
-      }));
+      nixosConfigurations = (nixpkgs.lib.genAttrs targets
+        (target: mkLinuxSystem {
+          inherit target;
+          module = hardwareModules."${target}";
+        }));
     };
 }
