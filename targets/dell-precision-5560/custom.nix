9870a291f5773d944a80d42d801b13867c264972
David Asabina <vid@bina.me>
2022-04-28 08:48:50 +0000

    feat: Install drivers for accel video playback

--- targets/dell-precision-5560/custom.nix
+++ targets/dell-precision-5560/custom.nix
@@ -3,10 +3,19 @@
 {
   hardware = {
     # https://nixos.wiki/wiki/OpenGL
+    # https://nixos.wiki/wiki/Accelerated_Video_Playback
     opengl = {
       enable = true;
       extraPackages = with pkgs; [
         mesa.drivers
+
+        # LIBVA_DRIVER_NAME=iHD
+        intel-media-driver
+
+        # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
+        vaapiIntel
+        vaapiVdpau
+        libvdpau-va-gl
       ];
     };
   };
