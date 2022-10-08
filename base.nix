4c3b60e4bc530e322a3b3151080cf970e8ef0fc1
David Asabina <vid@bina.me>
2022-10-08 22:55:06 +0000

    chore: expand TLP settings

--- base.nix
+++ base.nix
@@ -113,14 +113,40 @@ in
 
     tlp = {
       settings = {
+        #https://linrunner.de/tlp/settings/platform.html
+        PLATFORM_PROFILE_ON_AC = "performance";
+        PLATFORM_PROFILE_ON_BAT = "low-power";
+
         # https://discourse.nixos.org/t/how-to-switch-cpu-governor-on-battery-power/8446/4
+        # https://linrunner.de/tlp/settings/processor.html
+        CPU_SCALING_GOVERNOR_ON_AC = "performance";
         CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
-        CPU_SCALING_GOVERNOR_ON_AC = "powersave";
-        CPU_MAX_PERF_ON_BAT = 40;
+
+        CPU_ENERGY_PERF_POLICY_ON_AC = "balance_performance";
+        CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+
+        CPU_HWP_ON_AC = "balance_performance";
+        CPU_HWP_ON_BAT = "power";
+
+        CPU_MAX_PERF_ON_BAT = 30;
+
+        CPU_BOOST_ON_AC = 1;
+        CPU_BOOST_ON_BAT = 0;
 
         # https://github.com/linrunner/TLP/issues/122
-        WIFI_PWR_ON_AC = 1;
-        WIFI_PWR_ON_BAT = 5;
+        # https://linrunner.de/tlp/settings/network.html
+        WIFI_PWR_ON_AC = "off";
+        WIFI_PWR_ON_BAT = "on";
+
+        CPU_HWP_DYN_BOOST_ON_AC = 1;
+        CPU_HWP_DYN_BOOST_ON_BAT = 0;
+
+        SCHED_POWERSAVE_ON_AC = 0;
+        SCHED_POWERSAVE_ON_BAT = 1;
+
+        # https://linrunner.de/tlp/settings/runtimepm.html
+        RUNTIME_PM_ON_AC = "on";
+        RUNTIME_PM_ON_BAT = "auto";
       };
     };
   };
