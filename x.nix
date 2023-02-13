98e59a797e8001215d1b12542c3c97d4add63ddd
David Asabina <vid@bina.me>
2023-02-13 05:31:48 +0000

    chore: add slock and lock on lid close

--- x.nix
+++ x.nix
@@ -17,6 +17,10 @@
     xtruss
   ];
 
+  programs = {
+    slock = { enable = true; };
+  };
+
   services = {
     urxvtd = {
       enable = true;
@@ -48,6 +52,18 @@
     # https://nixos.wiki/wiki/Logind
     logind = {
       lidSwitch = "suspend";
+    };
+  };
+
+  systemd.services.lock-before-suspend = {
+    # https://www.jvt.me/posts/2019/12/03/lock-before-suspend-systemd/
+    description = "Lock X before suspend";
+    wantedBy = [ "suspend.target" "suspend-then-hibernate.target" ];
+    before = [ "sleep.target" ];
+    serviceConfig = {
+      User = config.my-config.handle;
+      Environment = "DISPLAY=:0";
+      ExecStart = ''${pkgs.slock}/bin/slock'';
     };
   };
 }
