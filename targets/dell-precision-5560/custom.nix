d7022040526cdf20eeff7cef1d976820e4b2ad32
David Asabina <vid@bina.me>
2023-03-04 18:08:35 +0000

    chore: remove older graphics drivers

--- targets/dell-precision-5560/custom.nix
+++ targets/dell-precision-5560/custom.nix
@@ -7,7 +7,6 @@
   ];
 
   nixpkgs.config.packageOverrides = pkgs: {
-    vaapiIntel = pkgs.vaapiIntel.override { enableHybridCodec = true; };
   };
 
   hardware = {
@@ -20,15 +19,8 @@
       driSupport32Bit = true;
 
       extraPackages = with pkgs; [
-        mesa.drivers
-
         # LIBVA_DRIVER_NAME=iHD
         intel-media-driver
-
-        # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
-        vaapiIntel
-        vaapiVdpau
-        libvdpau-va-gl
 
         # New additions
         vaapi-intel-hybrid
