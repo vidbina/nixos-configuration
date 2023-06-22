e2ff4173bb551451df8f8c83d985f0a25b567aea
David Asabina <vid@bina.me>
2023-06-22 19:05:24 +0000

    chore: Remove synaptics and list libinput

--- i3.nix
+++ i3.nix
@@ -6,13 +6,6 @@
   ];
 
   services.xserver = {
-    synaptics = {
-      enable = true;
-      horizEdgeScroll = false;
-      palmDetect = true;
-      twoFingerScroll = true;
-      vertEdgeScroll = false;
-    };
     windowManager = pkgs.lib.mkForce {
       default = "i3";
       i3 = {
