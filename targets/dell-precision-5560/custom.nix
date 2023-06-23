efeb1aa765db945ab296f979b1be7be79cbb6a57
David Asabina <vid@bina.me>
2023-06-23 12:07:42 +0000

    chore: List all drivers that we can imagine needing

--- targets/dell-precision-5560/custom.nix
+++ targets/dell-precision-5560/custom.nix
@@ -28,15 +28,30 @@
         mesa.drivers
         intel-media-driver # LIBVA_DRIVER_NAME=iHD
         vaapiIntel         # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
+        vaapiVdpau
+        libvdpau-va-gl
+      ];
+
+      extraPackages32 = with pkgs.pkgsi686Linux; [
+        # Installing linux firmware as per
+        # https://wiki.archlinux.org/title/Hardware_video_acceleration
+        linux-firmware
+        mesa.drivers
+        intel-media-driver # LIBVA_DRIVER_NAME=iHD
+        vaapiIntel         # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
+        vaapiVdpau
+        libvdpau-va-gl
       ];
     };
 
     # https://nixos.wiki/wiki/Nvidia
+    # our card is mux-less/non-MXM Optimus (shows up as 3D controller in lspci instead of VGA Controller)
     nvidia = {
       nvidiaSettings = true;
       modesetting.enable = true;
       powerManagement.enable = true;
       prime = {
+        #offload.enable = true;
         intelBusId = "PCI:0:2:0";
         nvidiaBusId = "PCI:1:0:0";
       };
@@ -47,5 +62,5 @@
 
   # Listing just the NVIDIA driver as per:
   # https://github.com/NixOS/nixpkgs/issues/108018
-  services.xserver.videoDrivers = [ "intel" "nvidia" ];
+  services.xserver.videoDrivers = [ "nvidia" "intel" ];
 }
