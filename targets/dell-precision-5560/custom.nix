e5246a2808f0bca97c29ed21df67a5513f3c0552
David Asabina <vid@bina.me>
2023-07-22 20:48:05 +0000

    chore: Format all .nix files

--- targets/dell-precision-5560/custom.nix
+++ targets/dell-precision-5560/custom.nix
@@ -8,8 +8,7 @@
     intel-gpu-tools
   ];
 
-  nixpkgs.config.packageOverrides = pkgs: {
-  };
+  nixpkgs.config.packageOverrides = pkgs: { };
 
   boot.loader.grub = {
     font = "${pkgs.dejavu_fonts}/share/fonts/truetype/DejaVuSansMono.ttf";
@@ -18,14 +17,13 @@
     # NOTE: From 2.06 to 2.12-rc1 missing shim symbol breaks the bootloader
     # see https://github.com/NixOS/nixpkgs/issues/243026
 
-    # Workaround: Produce normal boot menu on a working GRUB2 version
+    # Workaround: Produce normal boot menu on a unaffected GRUB2 version
 
     # Note that we can workaround by:
     # - prepping a NixOS pendrive
     # - booting into GRUB2 from the pendrive (v2.06)
     # - Run: set prefix=(hd1,gpt2)/grub
     # - Run: set root=(hd1,gpt2)
-    # - Run: set cmdpath=(hd1,gpt2)/efi/NixOS-boot
     # - Run: insmod normal
     # - Run: normal
     extraGrubInstallArgs = [
@@ -49,7 +47,7 @@
         linux-firmware
         mesa.drivers
         intel-media-driver # LIBVA_DRIVER_NAME=iHD
-        vaapiIntel         # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
+        vaapiIntel # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
         vaapiVdpau
         libvdpau-va-gl
       ];
@@ -60,7 +58,7 @@
         linux-firmware
         mesa.drivers
         intel-media-driver # LIBVA_DRIVER_NAME=iHD
-        vaapiIntel         # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
+        vaapiIntel # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
         vaapiVdpau
         libvdpau-va-gl
       ];
--- targets/dell-precision-5560/hardware-configuration.nix
+++ targets/dell-precision-5560/hardware-configuration.nix
@@ -5,7 +5,8 @@
 
 {
   imports =
-    [ (modulesPath + "/installer/scan/not-detected.nix")
+    [
+      (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
   boot.loader.grub = {
@@ -28,18 +29,19 @@
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/28ee814d-75d1-4673-87b3-2f1a66c1bce5";
+    {
+      device = "/dev/disk/by-uuid/28ee814d-75d1-4673-87b3-2f1a66c1bce5";
       fsType = "ext4";
     };
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/62CA-86FD";
+    {
+      device = "/dev/disk/by-uuid/62CA-86FD";
       fsType = "vfat";
     };
 
   swapDevices =
-    [ { device = "/dev/disk/by-uuid/d43e9ac4-b767-4da0-9e64-c33f51991fb8"; }
-    ];
+    [{ device = "/dev/disk/by-uuid/d43e9ac4-b767-4da0-9e64-c33f51991fb8"; }];
 
   powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
 }
